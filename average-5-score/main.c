7c00832d541301dccb3fbd9684f1bfeec2a8f999
AmiiirCom <amhoes1378@gmail.com>
2020-09-24 19:20:18 +0330

    Create main.c

---
+++ average-5-score/main.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+
+int main() {
+
+	float nomarat[5] ;
+	float jame_nomarat = 0;
+	float moadel = 0;
+	int i;
+
+	for (i = 0; i < 5; i++)
+
+	{
+		printf("\nEnter Score: ");
+
+		scanf("%f", &nomarat[i]);
+	}
+
+	printf("\n\n----- --------------------- ----- \n\n");
+
+	for (i = 0; i < 5; i++)
+	{
+		jame_nomarat = jame_nomarat + nomarat[i];
+	}
+	moadel = (jame_nomarat / 5);
+
+	printf("The sum is : %.2f \n", jame_nomarat);
+	printf("The average is : %.2f \n", moadel);
+
+
+   getchar();
+   getchar();
+   return 0;
+}
