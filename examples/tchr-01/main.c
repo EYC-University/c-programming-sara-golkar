b66b8d313c433f3c9a86856389465abfc6508530
AmiiirCom <amhoes1378@gmail.com>
2020-10-11 10:42:29 +0330

    Create main.c

---
+++ examples/tchr-01/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i, sum=0, num;
+    float ave;
+    const int n = 3;
+    for(i=0; i<n;i++){
+        printf("enter number %d: ", i+1);
+        scanf("%d", &num);
+        num = num*3;
+        sum += num;
+    }
+    ave = (float) sum/9;
+    printf("\n the average is: %6.2f ",ave);
+    return 0;
+}
