9aedcb34b73185f22c96a68c7059012170906e91
AmiiirCom <amhoes1378@gmail.com>
2020-10-18 09:02:33 +0330

    fix bugs and minor improvements

--- examples/example3-5/main.c
+++ examples/example3-5/main.c
@@ -5,9 +5,8 @@ int main()
 {
     int i,j;
     for (i=1; i<=5; i++){
-        for(j=1; j<=5; j++){
+        for(j=1; j<=5; j++)
             printf("%3d",i*j);
-        }
         printf("\n");
     }
 }
