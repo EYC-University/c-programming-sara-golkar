06c57931fd99b499ffa661e170d8a2d791c96d66
AmiiirCom <amhoes1378@gmail.com>
2020-10-25 16:29:10 +0330

    Update main.c

--- practices/season2/8/main.c
+++ practices/season2/8/main.c
@@ -5,7 +5,7 @@ int main()
     const int salary = 750000;
     const float increase_rate = 13.5;
     int res;
-    res = (salary * 13.5 / 100) * 12 * 2;
+    res = (salary * increase_rate / 100) * 12 * 2;
     printf("Annual increase rate for 2 specialists: %d", res);
     return 0;
 }
