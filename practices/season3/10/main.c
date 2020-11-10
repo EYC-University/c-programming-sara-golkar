e09713396cc87a1bac5ad012dd81e82466ad0e5b
AmiiirCom <amhoes1378@gmail.com>
2020-11-11 00:19:36 +0330

    Update main.c

--- practices/season3/10/main.c
+++ practices/season3/10/main.c
@@ -4,18 +4,18 @@
 
 int main()
 {
-  int i,j,n,x;
-  float result=0;
-  printf("Enter n: ");
-  scanf("%d",&n);
+    int i,j,n,x;
+    float result=0;
+    printf("Enter n: ");
+    scanf("%d",&n);
 
-  for (i=1;i<=n;i++)
-      {
-         x=1;
-         for(j=1;j<=i;j++)
+    for (i=1; i<=n; i++)
+    {
+        x=1;
+        for(j=1; j<=i; j++)
             x=x*j;
-         result=result+(float)1/x;
-      }
-  printf("\nResult is : %5.7f", result);
-  return 0;
+        result=result+(float)1/x;
+    }
+    printf("\nResult is : %5.7f", result);
+    return 0;
 }
