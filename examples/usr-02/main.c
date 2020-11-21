e58e40a5a9925d0ac72f2d0942c824ae9d14f39d
AmiiirCom <amhoes1378@gmail.com>
2020-11-21 23:58:01 +0330

    Create main.c

---
+++ examples/usr-02/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int x,y,z;
+int main()
+{
+    printf("please enter first number then press:");
+    scanf ("%d", &x);
+    printf ("please enter second number then press:");
+    scanf ("%d", &y);
+    printf ("x=%d, y=%d, x+y=%d\n", x, y, z=x+y);
+
+    printf("please enter first number then press:");
+    scanf ("%d", &x);
+    printf ("please enter second number then press:");
+    scanf ("%d", &y);
+    printf ("x=%d, y=%d, x-y=%d\n", x, y, z=x-y);
+
+    printf("please enter first number then press:");
+    scanf ("%d", &x);
+    printf ("please enter second number then press:");
+    scanf ("%d", &y);
+    printf ("x=%d, y=%d, x*y=%d\n", x, y, z=x*y);
+
+    printf("please enter first number then press:");
+    scanf ("%d", &x);
+    printf ("please enter second number then press:");
+    scanf ("%d", &y);
+    printf ("x=%d, y=%d, x/y=%d\n", x, y, z=x/y);
+}
